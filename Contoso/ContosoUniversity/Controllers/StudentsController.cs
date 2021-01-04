164e821f321a9d35369d1e999d3951272508af59
Mark Chenpingling <chenpingling@gmail.com>
2021-01-04 20:24:44 +0800

    Add paging to Students Index

--- Contoso/ContosoUniversity/Controllers/StudentsController.cs
+++ Contoso/ContosoUniversity/Controllers/StudentsController.cs
@@ -21,10 +21,27 @@ namespace ContosoUniversity
 
         // GET: Students
         //public async Task<IActionResult> Index(string sortOrder)
-        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        //public async Task<IActionResult> Index(string sortOrder, string searchString)
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? pageNumber)
         {
+            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
 
             ViewData["CurrentFilter"] = searchString;
 
@@ -55,7 +72,9 @@ namespace ContosoUniversity
                     students = students.OrderBy(s => s.LastName);
                     break;
             }
-            return View(await students.AsNoTracking().ToListAsync());
+            // return View(await students.AsNoTracking().ToListAsync());
+            int pageSize = 5;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Students/Details/5
